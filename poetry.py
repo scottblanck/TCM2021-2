21dc7f7fb0cbdd4ff03c77b4c2c77620704d0edc
Scott Blanck <scottblanck@gmail.com>
2021-03-17 11:27:16 -0500

    Last Haiku for the day

--- poetry.py
+++ poetry.py
@@ -1,25 +1,39 @@
 # Adding some poetry here
 # editing from IDLE
 
-print('My favorite Haikus...')
-print('make me relax...')
+print("My favorite Haikus...")
+print("make me relax...")
 
 print()
 
-print(''' 
+print(
+    """ 
 The wren
 Earns his living
 Noiselessly.
 
 - Kobayahsi Issa 
-''')
+"""
+)
 
 print()
 
-print(''' 
+print(
+    """ 
 From time to time
 The clouds give rest
 To the moon-beholders.
 
 - Matsuo Bashō
-''')
+"""
+)
+
+print(
+    """ 
+An old silent pond...
+A frog jumps into the pond,
+splash! Silence again.
+
+- Matsuo Bashō
+"""
+)
